2a8269e1004d1e4266a03ea6f3633340bc0d5954
acairesm <acairesm4777@gmail.com>
2026-04-08 17:53:25 -0300

    feat: adiciona componentes Avatar e CardPost

---
+++ components/CardPost/index.jsx
@@ -0,0 +1,20 @@
+import Image from "next/image"
+
+export const CardPost = () => {
+ return (
+    <article>
+        <header>
+            <figure>
+                <Image />
+            </figure>
+        </header>
+        <section>
+            titulo
+            texto
+        </section>
+        <footer>
+            <Avatar   />
+        </footer>
+    </article>
+ ) 
+} 
