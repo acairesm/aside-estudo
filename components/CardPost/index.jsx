a4cb6f8ba983a9388408a28ed955289b67d8a14d
acairesm <acairesm4777@gmail.com>
2026-04-10 09:02:45 -0300

    refactor: melhora a formatação e o estilo do componente CardPost

--- components/CardPost/index.jsx
+++ components/CardPost/index.jsx
@@ -7,18 +7,21 @@ export const CardPost = ({ post }) => {
     <article className="rounded-lg bg-[#171D1F]">
       <header className=" rounded-t-lg bg-[#888] p-4">
         <figure className="m-0">
-          <Image 
-          src={post.cover} 
-          alt={ `Imagem do post  ${ post.title }` } 
-          width={800} 
-          height={450} />
+          <Image
+            src={post.cover}
+            alt={`Imagem do post  ${post.title}`}
+            width={800}
+            height={450}
+          />
         </figure>
       </header>
-      <section className="text-[#BCBCBC] p-4">
-        <h2>{post.title}</h2>
-        <p>{post.body}</p>
+      <section className="text-[#BCBCBC] p-4 flex flex-col gap-2">
+        <h2 className="text-[18px] not-italic font-semibold leading-[150%] m-0 ">
+          {post.title}
+        </h2>
+        <p className="text-[15px] not-italic font-normal  leading-[150%] m-0 flex-grow">{post.body}</p>
       </section>
-      <footer>
+      <footer className="p-4">
         <Avatar name={post.author.username} imageSrc={post.author.avatar} />
       </footer>
     </article>
