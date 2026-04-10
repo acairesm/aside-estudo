96cfd18f202b07045f8016b18868a212c5d5f4b4
acairesm <acairesm4777@gmail.com>
2026-04-10 08:22:22 -0300

    feat: adiciona suporte a fontes personalizadas no layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,18 @@
 import type { Metadata } from "next";
 import { Aside } from '@/components/Aside';
 import './globals.css'
+import {Prompt } from 'next/font/google';
+
 
 export const metadata: Metadata = {
   title: "  Code Connect ",
   description: "Uma Rede Social Para devs",
 };
+
+const prompt  = Prompt ({
+  subsets: ['latin'],
+  weight: ['400', '600'],
+})
 
 export default function RootLayout({
   children,
@@ -14,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={prompt.className}>
         <div className="w-[1200px] mx-auto my-14 max-w-[90%] flex gap-7 h-screen">
           <Aside/>
           {children}
